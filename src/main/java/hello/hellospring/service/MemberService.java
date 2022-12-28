3e2eb40fc58cfdd183d68e10cd2348f3d1bce762
simon <simon0823@naver.com>
2022-12-28 23:58:11 +0900

    스프링 DB 접근 기술

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -5,10 +5,12 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public class MemberService {
     private final MemberRepository memberRepository;
 
@@ -24,7 +26,6 @@ public class MemberService {
         memberRepository.save(member);
         return member.getId();
     }
-    
 
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
